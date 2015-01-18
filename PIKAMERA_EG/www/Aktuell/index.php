58dd792451c2bea08beaade282735ddf1dbb1d75
Emilleopold <mschuessler@ntailserver.ntail.private>
2015-01-18 17:06:16 +0100

    EIKAMERA EG hinzugefügt

---
+++ PIKAMERA_EG/www/Aktuell/index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 'On');
+// ini_set('error_log','/var/log/phperror1.log');
+
+if (isset($_POST['PUNDT']) AND !empty($_POST['PUNDT'])) {
+ if ($_POST['PUNDT'] == "Rund") { shell_exec("./rundfahrt.py"); }
+ if ($_POST['PUNDT'] == "Treppe") { shell_exec("./treppe.py"); }
+ if ($_POST['PUNDT'] == "Links") { shell_exec("./links.py"); }
+ if ($_POST['PUNDT'] == "Rechts") { shell_exec("./rechts.py"); }
+ if ($_POST['PUNDT'] == "Oben") { shell_exec("./oben.py"); }
+ if ($_POST['PUNDT'] == "Unten") { shell_exec("./unten.py"); }
+}
+
+// echo 'Und jetzt der Fehler: ';
+// echo gibtsnicht(); 
+
+?>
+
+<title></title>
+</head>
+<body>
+<form method="post">
+  <table style="width: 75%; text-align: left; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="2">
+    <tr>
+      <th style="text-align: center;">RUNDFAHRT</th>
+      <th style="text-align: center;">TREPPE</th>
+      <th style="text-align: center;">LINKS</th>
+      <th style="text-align: center;">RECHTS</th>
+      <th style="text-align: center;">OBEN</th>
+      <th style="text-align: center;">UNTEN</th>
+    </tr>
+    <tr>
+      <td style="text-align: center;"><button type="submit" name="PUNDT" value="Rund">Rundfahrt</button></td>
+      <td style="text-align: center;"><button type="submit" name="PUNDT" value="Treppe">Treppe</button></td>
+      <td style="text-align: center;"><button type="submit" name="PUNDT" value="Links">Links</button></td>
+      <td style="text-align: center;"><button type="submit" name="PUNDT" value="Rechts">Rechts</button></td>
+      <td style="text-align: center;"><button type="submit" name="PUNDT" value="Oben">Oben</button></td>
+      <td style="text-align: center;"><button type="submit" name="PUNDT" value="Unten">Unten</button></td>
+    </tr>
+  </table>
+</form>
+   <iframe src="http://192.168.20.232:8081/?action=stream" height="800" width="800" frameborder="0"></iframe>
+</body>
+</html> 
